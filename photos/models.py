42eaf9cf4eec4f6cd2affb0b5fc45b67d1f5bd05
Alexander Nepein <alekswonder@yandex.ru>
2023-07-23 00:39:01 +0500

    Add Photo manager

--- photos/models.py
+++ photos/models.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class AbstractLocation(models.Model):
     name = models.CharField(
         max_length=255,
-        verbose_name='Название'
+        verbose_name='Название',
     )
     founding_date = models.DateField(
         verbose_name='Дата основания'
@@ -36,7 +36,7 @@ class AbstractLocation(models.Model):
 class AbstractThing(models.Model):
     title = models.CharField(
         verbose_name='Название',
-        max_length=255
+        max_length=255,
     )
     description = models.TextField(
         verbose_name='Описание',
@@ -60,6 +60,9 @@ class Item(AbstractThing):
         default_related_name = 'items'
         verbose_name = 'Вещь'
         verbose_name_plural = 'Вещи'
+
+    def __str__(self):
+        return self.title
 
 
 class Country(AbstractLocation):
@@ -100,12 +103,17 @@ class Country(AbstractLocation):
         verbose_name = 'Страна'
         verbose_name_plural = 'Страны'
 
+    def __str__(self):
+        return self.name
+
 
 class City(AbstractLocation):
     country = models.ForeignKey(
         Country,
         on_delete=models.CASCADE,
         verbose_name='Страна',
+        blank=True,
+        null=True
     )
     item = models.ForeignKey(
         Item,
@@ -119,12 +127,39 @@ class City(AbstractLocation):
         default_related_name = 'cities'
         verbose_name = 'Город'
         verbose_name_plural = 'Города'
+        constraints = [
+            models.UniqueConstraint(fields=('name', 'founding_date', 'demonym', 'ruler', 'area', 'population'),
+                                    name='unique_city')
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class PhotoManager(models.Manager):
+    def approved_photos(self):
+        return self.filter(is_approved=True)
+
+    def approved_photos_for_entity(self, entity_type, entity_id):
+        if entity_type == 'country':
+            return self.filter(country_id=entity_id, is_approved=True)
+        elif entity_type == 'city':
+            return self.filter(city_id=entity_id, is_approved=True)
+        elif entity_type == 'item':
+            return self.filter(item_id=entity_id, is_approved=True)
+        elif entity_type == 'user':
+            return self.filter(user_id=entity_id, is_approved=True)
+        else:
+            return self.none()
+
+    def unapproved_photos(self):
+        return self.filter(is_approved=False)
 
 
 class Photo(AbstractThing):
     image = models.ImageField(
         verbose_name='Картинка',
-        upload_to='photos/',
+        upload_to='photos/images/',
         null=True,
         blank=True
     )
@@ -149,19 +184,17 @@ class Photo(AbstractThing):
         null=True,
         blank=True
     )
-    owner = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Владелец',
-        null=True,
-        blank=True
-    )
     is_approved = models.BooleanField(
         default=True,
         verbose_name='Одобрено администратором'
     )
 
+    photos_manager = PhotoManager()
+
     class Meta:
         default_related_name = 'photos'
         verbose_name = 'Фотография'
         verbose_name_plural = 'Фотографии'
+
+    def __str__(self):
+        return self.title
