6b4bbfc935fdf4b756630d3edbfbd2e68203965f
Alexander Nepein <alekswonder@yandex.ru>
2023-07-22 14:40:47 +0500

    Add template for models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Country(models.Model):
+    ...
+
+    class Meta:
+        ...
+
+
+class City(models.Model):
+    country = models.ForeignKey(
+        Country,
+        ...
+    )
+
+    class Meta:
+        default_related_name = 'cities'
+
+
+class Item(models.Model):
+    ...
+
+
+class Photo(models.Model):
+    country = models.ForeignKey(
+        Country,
+        ...
+    )
+    city = models.ForeignKey(
+        City,
+        ...
+    )
+    item = models.ForeignKey(
+        Item,
+        ...
+    )
+    owner = models.ForeignKey(
+        User,
+        ...
+    )
+
+    class Meta:
+        default_related_name = 'photos'
